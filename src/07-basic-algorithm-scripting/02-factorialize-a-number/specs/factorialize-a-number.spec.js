0f3d0d93abea5fded77b7b6298e440ec546dcc1a
Isaac Ramirez <isaac.ramirez@unosquare.com>
2017-10-08 19:27:34 -0500

    solve factorialize a number

---
+++ src/07-basic-algorithm-scripting/02-factorialize-a-number/specs/factorialize-a-number.spec.js
@@ -0,0 +1,25 @@
+const expect = require('expect.js');
+const factorialize = require('../factorialize-a-number');
+
+const tests = [
+  { input: 0, result: 1 },
+  { input: 1, result: 1 },
+  { input: 2, result: 2 },
+  { input: 3, result: 6 },
+  { input: 4, result: 24 },
+  { input: 5, result: 120 },
+  { input: 10, result: 3628800 },
+  { input: 20, result: 2432902008176640000 },
+];
+
+describe('Factorialize A Number Unit Tests:', function () {
+  it('should expose a function', function () {
+    expect(factorialize).to.be.a('function');
+  });
+
+  tests.forEach(({ input, result }) => {
+    it(`should factorialize number: ${input}, result: ${result}`, function () {
+      expect(factorialize(input)).to.equal(result);
+    });
+  });
+});
