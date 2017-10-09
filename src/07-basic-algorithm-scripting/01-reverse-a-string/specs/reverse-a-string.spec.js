7ef7890c3a6117c690d06c48c0c3bd0ddecfcf1c
Isaac Ramirez <isaac.ramirez@unosquare.com>
2017-10-08 19:27:22 -0500

    solve reverse a string

---
+++ src/07-basic-algorithm-scripting/01-reverse-a-string/specs/reverse-a-string.spec.js
@@ -0,0 +1,14 @@
+const expect = require('expect.js');
+const reverseString = require('../reverse-a-string');
+
+describe('Reverse A String Unit Tests:', function () {
+  it('should expose a function', function () {
+    expect(reverseString).to.be.a('function');
+  });
+
+  it('should reverse a string', function () {
+    expect(reverseString('Hello')).to.equal('olleH');
+    expect(reverseString('12345')).to.equal('54321');
+    expect(reverseString('eme')).to.equal('eme');
+  });
+});
